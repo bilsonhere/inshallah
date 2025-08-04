f5bd7f1fb37cf6ee0c121b595408e069638569f4
bilsonhere <121447291+bilsonhere@users.noreply.github.com>
2025-08-04 10:26:41 +0545

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+   apiKey: "AIzaSyCUWnVPUF4cM0e1AbKVHnk7HjD2VO9xRho",
+
+  authDomain: "fetch-56579.firebaseapp.com",
+
+  databaseURL: "https://fetch-56579-default-rtdb.firebaseio.com",
+
+  projectId: "fetch-56579",
+
+  storageBucket: "fetch-56579.firebasestorage.app",
+
+  messagingSenderId: "1048781041087",
+
+  appId: "1:1048781041087:web:c69453a8181239f999450a",
+
+  measurementId: "G-8S6ESBBYH9"
+
+
+
+};
